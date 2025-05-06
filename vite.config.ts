df946a14fd1abe110896f0b4dbd0b58835f81631
Ubuntu <ubuntu@ip-172-31-92-123.ec2.internal>
2025-05-06 08:35:34 +0000

    change viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Bind to all interfaces so it's accessible externally
+    port: 5173       // Optional: can be omitted if default is okay
+  }
 })
+
