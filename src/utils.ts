ea8f816c6a21163d1269fa99c04d49c948062f97
Coding-Pandey <manaslinux01@gmail.com>
2025-11-14 11:29:11 +0530

    test-commit-branchh

--- src/utils.ts
+++ src/utils.ts
@@ -7,6 +7,7 @@ export const deleteS3Files = async (uuid: string) => {
       `/api/delete_s3_file?file_uuid=${uuid}`
     );
     console.log("dse");
+    console.log("dse");
     return response;
   } catch (error) {
     throw error;
